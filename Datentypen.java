3ca7e5eed0b337f862a1baa2b0d23aeee61310c5
Dorion65 <63191291+Dorion65@users.noreply.github.com>
2020-04-05 23:58:50 +0200

    Update Datentypen.java
    damit inhalt existiert

--- Datentypen.java
+++ Datentypen.java
@@ -2,7 +2,24 @@
 public class Datentypen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean jain = true;
+		if(jain == true)
+		{
+			char a = 'A';
+			char b = '1';
+			String BOOL = "True";
+			System.out.println(BOOL + a + b);
+			jain = false;
+		}
+		
+		if(jain == false)
+		{
+			int i = 22;
+			long l = i;
+			int ii = (int) l; //casting long -> int
+			System.out.println("False" + ii);
+			jain = true;
+		}
 
 	}
 
